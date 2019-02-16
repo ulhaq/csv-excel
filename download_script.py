92224c62ff8c55b26be620e574d0513619b6c0d7
Muhammad Umar Ulhaq <ullhaq@hotmail.com>
2019-02-16 13:41:56 +0100

    if-condition refactoring

--- download_script.py
+++ download_script.py
@@ -4,10 +4,9 @@ from webget import download
 for arg in sys.argv[1:]:
     args = arg.split()
     url = args[0]
+    location = None
     
-    if len(args) == 2:
+    if len(args) > 1:
         location = args[1]
-    else:
-        location = None
     
     download(url, location)
