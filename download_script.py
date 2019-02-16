2e91d446d1c78812967f3e2be212d71175f84580
Muhammad Umar Ulhaq <ullhaq@hotmail.com>
2019-02-16 13:29:13 +0100

    location param added

--- download_script.py
+++ download_script.py
@@ -1,5 +1,13 @@
 import sys
 from webget import download
 
-for url in sys.argv[1:]:
-    download(url)
+for arg in sys.argv[1:]:
+    args = arg.split()
+    url = args[0]
+    
+    if len(args) == 2:
+        location = args[1]
+    else:
+        location = None
+    
+    download(url, location)
